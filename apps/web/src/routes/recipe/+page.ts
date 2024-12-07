f8e9bce9493773f6f999914884472c51df18a1db
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2024-12-07 09:52:44 +0100

    refactor: cleanup

--- apps/web/src/routes/recipe/+page.ts
+++ apps/web/src/routes/recipe/+page.ts
@@ -3,6 +3,5 @@ import { getRecipes } from '$lib/getRecipes';
 
 export async function load(): Promise<{ recipes: Recipe[] }> {
   const recipes = await getRecipes();
-  console.log('recipes in load:', recipes); // Debug-log
   return { recipes };
 }
