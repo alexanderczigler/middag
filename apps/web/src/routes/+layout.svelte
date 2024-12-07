5ad0e05ceac8c4044c91ae995e3730ab7de4e7c0
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2024-12-07 08:48:29 +0100

    feat(menu): mark current page

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
+  import { page } from '$app/stores';
   import '../app.css';
+
   const { children } = $props();
 </script>
 
 <ul class="menu">
   <li>
-    <a href="/">Meny</a>
+    <a href="/" class:active={$page.url.pathname === '/'}>Meny</a>
   </li>
   <li>
-    <a href="/shoppingList">Inköpslista</a>
+    <a href="/shoppingList" class:active={$page.url.pathname === '/shoppingList'}>Inköpslista</a>
   </li>
   <li>
-    <a href="/recipe">Recept</a>
+    <a href="/recipe" class:active={$page.url.pathname.startsWith('/recipe')}>Recept</a>
   </li>
 </ul>
 
@@ -32,5 +34,9 @@
   ul.menu li {
     list-style-type: none;
     margin: 1em 0;
+  }
+
+  ul.menu a.active {
+    font-weight: bold;
   }
 </style>
