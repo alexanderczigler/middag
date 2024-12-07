f6e88db97600f96a9966ac4c3fb5c51012dd62a9
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2024-12-07 08:58:08 +0100

    refactor: fix typing without using unknown

--- apps/web/src/lib/getRecipes.ts
+++ apps/web/src/lib/getRecipes.ts
@@ -1,15 +1,20 @@
 import type { Recipe } from './types';
 
 export async function getRecipes(): Promise<Recipe[]> {
-  const modules = import.meta.glob('/src/lib/recipes/*.json');
+  const modules = import.meta.glob('/src/lib/recipes/*.json') as Record<
+    string,
+    () => Promise<{ default: Recipe }>
+  >;
 
   const recipes: Recipe[] = [];
 
+  // Load each recipe found in the path.
   for (const path in modules) {
     const module = await modules[path]();
-    recipes.push(module.default as Recipe); // Säker typ-cast till Recipe
+    recipes.push(module.default);
   }
 
+  // Set slug from the recipe title.
   recipes.forEach((recipe) => {
     recipe.slug = recipe.slug || recipe.title.toLowerCase().replace(/ /g, '-');
   });
