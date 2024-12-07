7deac15bacfae8fd24faa53af038a075cbfe19d1
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2024-12-07 08:44:15 +0100

    feat(menu): some styling

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,16 +1,36 @@
 <script lang="ts">
-	import '../app.css';
-	const { children } = $props();
+  import '../app.css';
+  const { children } = $props();
 </script>
 
-<ul>
-	<li>
-		<a href="/">Meny</a>
-		<a href="/shoppingList">Inköpslista</a>
-		<a href="/recipe">Recept</a>
-	</li>
+<ul class="menu">
+  <li>
+    <a href="/">Meny</a>
+  </li>
+  <li>
+    <a href="/shoppingList">Inköpslista</a>
+  </li>
+  <li>
+    <a href="/recipe">Recept</a>
+  </li>
 </ul>
 
 <hr />
 
 {@render children()}
+
+<style>
+  ul.menu {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: space-evenly;
+    align-items: baseline;
+    align-content: stretch;
+  }
+
+  ul.menu li {
+    list-style-type: none;
+    margin: 1em 0;
+  }
+</style>
