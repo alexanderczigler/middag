ed8d0b922e94b3bd533e800c82292e134cb3c1f3
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2025-01-05 16:34:08 +0100

    chore: improve types, update recipes

--- apps/web/src/lib/types.ts
+++ apps/web/src/lib/types.ts
@@ -10,10 +10,25 @@ export type Recipe = {
 export type Ingredient = {
   name: string;
   quantity: number;
-  unit: 'dl' | '';
+  unit: Unit;
 };
 
-export type Unit = 'dl' | 'krm' | 'msk' | 'g' | 'nypa' | 'tsk';
+export type Unit =
+  | 'dl' // Deciliter
+  | 'krm' // Kryddmått
+  | 'msk' // Matsked
+  | 'g' // Gram
+  | 'nypa' // Nypa
+  | 'tsk' // Tesked
+  | 'st' // Styck
+  | 'kg' // Kilogram
+  | 'ml' // Milliliter
+  | 'cl' // Centiliter
+  | 'l' // Liter
+  | 'förp' // Förpackning
+  | 'pkt' // Paket
+  | 'klyfta' // För t.ex. vitlök eller citron
+  | 'skiva'; // För t.ex. bröd eller ost
 
 export type Menu = {
   [date: string]: string;
