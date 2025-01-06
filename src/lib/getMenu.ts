0ecafd3c64cd837e417978d06c2fefc8649b3d4c
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2025-01-06 23:47:09 +0100

    chore: reorder menu

--- src/lib/getMenu.ts
+++ src/lib/getMenu.ts
@@ -2,11 +2,11 @@ import type { Menu } from './types/menu';
 
 export function getMenu(): Menu {
   return {
-    '2025-01-06': 'pasta-pesto',
-    '2025-01-07': 'broccoligratäng',
-    '2025-01-08': 'ugnsbakad-lax',
-    '2025-01-09': 'svamprisotto',
+    '2025-01-07': 'pasta-pesto',
+    '2025-01-08': 'broccoligratäng',
+    '2025-01-09': 'ugnsbakad-lax',
     '2025-01-10': 'caesarsallad',
-    '2025-01-11': 'spenat-och-fetaostpaj'
+    '2025-01-11': 'spenat-och-fetaostpaj',
+    '2025-01-12': 'svamprisotto'
   };
 }
