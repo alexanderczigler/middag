887d550df4392ecab65f1c54da70a9d43fecb874
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2025-01-06 12:14:15 +0100

    chore: simplify; only plan dinners

--- apps/web/src/lib/getMenu.ts
+++ apps/web/src/lib/getMenu.ts
@@ -2,9 +2,6 @@ import type { Menu } from './types/menu';
 
 export function getMenu(): Menu {
   return {
-    '2025-01-06': {
-      lunch: 'daal',
-      dinner: 'pasta-pesto'
-    }
+    '2025-01-06': 'pasta-pesto'
   };
 }
--- apps/web/src/lib/types/menu.ts
+++ apps/web/src/lib/types/menu.ts
@@ -1,10 +1,5 @@
 import type { Recipes } from './recipes';
 
-export type Meals = {
-  lunch: Recipes;
-  dinner: Recipes;
-};
-
 export type Menu = {
-  [date: string]: Meals;
+  [date: string]: Recipes;
 };
