83e56e56c8e3c2e58d3fbfaf230796d79432c8b0
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2025-01-12 10:16:28 +0100

    fix: adjust menu

--- src/lib/getMenu.ts
+++ src/lib/getMenu.ts
@@ -4,10 +4,10 @@ export function getMenu(): Menu {
   return {
     '2025-01-13': 'ugnsbakad-lax',
     '2025-01-14': 'grekisk-sallad',
-    '2025-01-15': 'tofu-stroganoff',
+    '2025-01-15': 'spenat-och-fetaostpaj',
     '2025-01-16': 'daal',
     '2025-01-17': 'mapu-tofu',
-    '2025-01-18': 'pasta-pesto',
+    '2025-01-18': 'tofu-stroganoff',
     '2025-01-19': 'caesarsallad'
   };
 }
