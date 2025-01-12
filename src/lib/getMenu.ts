db0c83283f0e6885a614026e92ac6671ccda3326
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2025-01-12 10:05:59 +0100

    feat: update menu

--- src/lib/getMenu.ts
+++ src/lib/getMenu.ts
@@ -2,11 +2,12 @@ import type { Menu } from './types/menu';
 
 export function getMenu(): Menu {
   return {
-    '2025-01-07': 'pasta-pesto',
-    '2025-01-08': 'broccoligratäng',
-    '2025-01-09': 'ugnsbakad-lax',
-    '2025-01-10': 'caesarsallad',
-    '2025-01-11': 'spenat-och-fetaostpaj',
-    '2025-01-12': 'svamprisotto'
+    '2025-01-13': 'ugnsbakad-lax',
+    '2025-01-14': 'grekisk-sallad',
+    '2025-01-15': 'tofu-stroganoff',
+    '2025-01-16': 'daal',
+    '2025-01-17': 'mapu-tofu',
+    '2025-01-18': 'pasta-pesto',
+    '2025-01-19': 'caesarsallad'
   };
 }
