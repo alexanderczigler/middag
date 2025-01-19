4950007cd0817c23b758eb3795cc61837d0c5fc9
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2025-01-19 09:22:23 +0100

    feat: next week

--- src/lib/getMenu.ts
+++ src/lib/getMenu.ts
@@ -2,12 +2,12 @@ import type { Menu } from './types/menu';
 
 export function getMenu(): Menu {
   return {
-    '2025-01-13': 'ugnsbakad-lax',
-    '2025-01-14': 'grekisk-sallad',
-    '2025-01-15': 'spenat-och-fetaostpaj',
-    '2025-01-16': 'daal',
-    '2025-01-17': 'mapu-tofu',
-    '2025-01-18': 'tofu-stroganoff',
-    '2025-01-19': 'caesarsallad'
+    '2025-01-19': 'caesarsallad',
+    '2025-01-20': 'tofu-stroganoff',
+    '2025-01-21': 'ugnsbakad-lax',
+    '2025-01-22': 'tonfisksallad',
+    '2025-01-23': 'pasta-pesto',
+    '2025-01-24': 'spenat-och-fetaostpaj',
+    '2025-01-25': 'grekisk-sallad'
   };
 }
