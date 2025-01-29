80bc0bafae729d268cce2e301b0aa8df840fcd13
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2025-01-29 19:14:53 +0100

    chore: remove previous week

--- src/lib/getMenu.ts
+++ src/lib/getMenu.ts
@@ -1,13 +1,6 @@
 import type { Menu } from './types/menu';
 
 const MENU: Menu = {
-  '2025-01-19': 'caesarsallad',
-  '2025-01-20': 'tofu-stroganoff',
-  '2025-01-21': 'ugnsbakad-lax',
-  '2025-01-22': 'tonfisksallad',
-  '2025-01-23': 'pasta-pesto',
-  '2025-01-25': 'spenat-och-fetaostpaj',
-  '2025-01-26': 'grekisk-sallad',
   '2025-01-27': 'broccoligratäng',
   '2025-01-28': 'ugnsbakad-lax',
   '2025-01-29': 'chicken-tikka-masala',
