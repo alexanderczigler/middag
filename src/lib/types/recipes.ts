b9e67f18d0b10767892652db04b2cddbcad06a7e
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2025-02-03 17:01:31 +0100

    feat: add recipe

--- src/lib/types/recipes.ts
+++ src/lib/types/recipes.ts
@@ -1,3 +1,3 @@
 // This file is auto-generated. Do not edit manually.
 
-export type Recipes = 'bouillabaisse' | 'broccoligratäng' | 'caesarsallad' | 'chicken-tikka-masala' | 'currykyckling' | 'daal' | 'färdig-risotto' | 'grekisk-sallad' | 'kinesisk-gurksallad' | 'kokoskyckling' | 'krämig-grönkål' | 'krämig-lax' | 'mapu-tofu' | 'pasta-pesto' | 'pestolasagne' | 'räkrisotto' | 'skaldjursomelett' | 'spenat-och-fetaostpaj' | 'svamprisotto' | 'tofu-stroganoff' | 'tonfisksallad' | 'tortellini-con-brodo' | 'ugnsbakad-lax';
+export type Recipes = 'bouillabaisse' | 'broccoligratäng' | 'caesarsallad' | 'chicken-tikka-masala' | 'currykyckling' | 'daal' | 'färdig-risotto' | 'grekisk-sallad' | 'kinesisk-gurksallad' | 'kokoskyckling' | 'krämig-grönkål' | 'krämig-lax' | 'mapu-tofu' | 'pasta-pesto' | 'pasta-tomatsås' | 'pestolasagne' | 'räkrisotto' | 'skaldjursomelett' | 'spenat-och-fetaostpaj' | 'svamprisotto' | 'tofu-stroganoff' | 'tonfisksallad' | 'tortellini-con-brodo' | 'ugnsbakad-lax';
