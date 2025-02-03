1753fd6705fd2c9f736b6f6fccff3b3c4b418d3b
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2025-02-03 17:01:14 +0100

    feat: update weekly menu

--- src/lib/getMenu.ts
+++ src/lib/getMenu.ts
@@ -1,13 +1,13 @@
 import type { Menu } from './types/menu';
 
 const MENU: Menu = {
-  '2025-01-27': 'broccoligratäng',
-  '2025-01-28': 'ugnsbakad-lax',
-  '2025-01-29': 'chicken-tikka-masala',
-  '2025-01-30': 'pasta-pesto',
-  '2025-02-01': 'tonfisksallad',
-  '2025-02-02': 'caesarsallad',
-  '2025-02-03': 'tofu-stroganoff'
+  '2025-02-02': 'tofu-stroganoff',
+  '2025-02-03': 'mapu-tofu', // Måndag
+  '2025-02-04': 'daal',
+  '2025-02-05': 'färdig-risotto',
+  '2025-02-06': 'tonfisksallad',
+  '2025-02-07': 'ugnsbakad-lax',
+  '2025-02-08': 'pasta-tomatsås'
 };
 
 export function getMenu(from?: string): Menu {
