86a044aeed97737a5991b8b308d4313dad45c962
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2025-02-05 08:18:57 +0100

    feat: rearrange week

--- src/lib/getMenu.ts
+++ src/lib/getMenu.ts
@@ -1,26 +1,26 @@
-import type { Menu } from './types/menu';
+import type { Menu } from "./types/menu";
 
 const MENU: Menu = {
-  '2025-02-02': 'tofu-stroganoff',
-  '2025-02-03': 'mapu-tofu', // Måndag
-  '2025-02-04': 'daal',
-  '2025-02-05': 'färdig-risotto',
-  '2025-02-06': 'tonfisksallad',
-  '2025-02-07': 'ugnsbakad-lax',
-  '2025-02-08': 'pasta-tomatsås'
+	"2025-02-02": "tofu-stroganoff",
+	"2025-02-04": "mapu-tofu",
+	"2025-02-05": "daal",
+	"2025-02-06": "tonfisksallad",
+	"2025-02-07": "ugnsbakad-lax",
+	"2025-02-08": "pasta-tomatsås",
+	"2025-02-09": "färdig-risotto",
 };
 
 export function getMenu(from?: string): Menu {
-  if (from) {
-    const fromDate = new Date(from);
-    const menu: Menu = {};
-    Object.entries(MENU).forEach(([date, recipe]) => {
-      if (new Date(date) >= fromDate) {
-        menu[date] = recipe;
-      }
-    });
-    return menu;
-  } else {
-    return MENU;
-  }
+	if (from) {
+		const fromDate = new Date(from);
+		const menu: Menu = {};
+		Object.entries(MENU).forEach(([date, recipe]) => {
+			if (new Date(date) >= fromDate) {
+				menu[date] = recipe;
+			}
+		});
+		return menu;
+	} else {
+		return MENU;
+	}
 }
