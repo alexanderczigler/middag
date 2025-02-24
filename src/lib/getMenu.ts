78dd29e0d7faa3a164a1eb535a307448acf720e5
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2025-02-24 20:15:49 +0100

    chore: weekly plan

--- src/lib/getMenu.ts
+++ src/lib/getMenu.ts
@@ -1,12 +1,9 @@
 import type { Menu } from './types/menu';
 
 const MENU: Menu = {
-  '2025-02-19': 'tonfisksallad',
-  '2025-02-20': 'tofu-stroganoff',
-  '2025-02-21': 'ugnsbakad-lax',
-  '2025-02-22': 'svamprisotto',
-  '2025-02-23': 'currykyckling',
-  '2025-02-24': 'grekisk-sallad'
+  '2025-02-25': 'grekisk-sallad',
+  '2025-03-01': 'mapu-tofu',
+  '2025-03-02': 'broccoligratäng'
 };
 
 export function getMenu(from?: string): Menu {
