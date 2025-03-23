381fad9fa577f8154ec478d56896ac89afc29419
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2025-03-23 14:08:00 +0100

    chore: plan and add holy caesar

--- src/lib/getMenu.ts
+++ src/lib/getMenu.ts
@@ -1,26 +1,25 @@
-import type { Menu } from './types/menu';
+import type { Menu } from "./types/menu";
 
 const MENU: Menu = {
-  '2025-03-16': 'pokebowl',
-  '2025-03-17': 'chana-daal',
-  '2025-03-18': 'ugnsbakad-lax',
-  '2025-03-19': 'tonfisksallad',
-  '2025-03-20': 'mapu-tofu',
-  '2025-03-21': 'broccoligratäng',
-  '2025-03-22': 'sötpotatisbowl'
+	"2025-03-24": "holy-caesar",
+	"2025-03-25": "ugnsbakad-lax",
+	"2025-03-26": "pokebowl",
+	"2025-03-27": "chana-daal",
+	"2025-03-28": "mexicali",
+	"2025-03-29": "chicken-tikka-masala",
 };
 
 export function getMenu(from?: string): Menu {
-  if (from) {
-    const fromDate = new Date(from);
-    const menu: Menu = {};
-    for (const [date, recipe] of Object.entries(MENU)) {
-      if (new Date(date) >= fromDate) {
-        menu[date] = recipe;
-      }
-    }
-    return menu;
-  }
+	if (from) {
+		const fromDate = new Date(from);
+		const menu: Menu = {};
+		for (const [date, recipe] of Object.entries(MENU)) {
+			if (new Date(date) >= fromDate) {
+				menu[date] = recipe;
+			}
+		}
+		return menu;
+	}
 
-  return MENU;
+	return MENU;
 }
