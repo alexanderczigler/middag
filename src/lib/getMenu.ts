f2ea7735d98882813e62da87ee451473023923d1
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2025-03-23 14:10:26 +0100

    chore: switch dates

--- src/lib/getMenu.ts
+++ src/lib/getMenu.ts
@@ -3,10 +3,10 @@ import type { Menu } from "./types/menu";
 const MENU: Menu = {
 	"2025-03-24": "holy-caesar",
 	"2025-03-25": "ugnsbakad-lax",
-	"2025-03-26": "pokebowl",
+	"2025-03-26": "chicken-tikka-masala",
 	"2025-03-27": "chana-daal",
 	"2025-03-28": "mexicali",
-	"2025-03-29": "chicken-tikka-masala",
+	"2025-03-29": "pokebowl",
 };
 
 export function getMenu(from?: string): Menu {
