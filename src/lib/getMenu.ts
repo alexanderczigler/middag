572f6c6a2a8d791c31569b3ed3bd88c9b843e3a9
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2025-04-24 18:40:09 +0200

    chore(menu): plan

--- src/lib/getMenu.ts
+++ src/lib/getMenu.ts
@@ -1,11 +1,12 @@
 import type { Menu } from "./types/menu";
 
 const MENU: Menu = {
-	"2025-04-15": "holy-caesar",
-	"2025-04-16": "tonfisksallad",
-	"2025-04-17": "currykyckling",
-	"2025-04-18": "pokebowl",
-	"2025-04-21": "mexicali",
+	"2025-04-25": "broccoligratäng",
+	"2025-04-26": "chana-daal",
+	"2025-04-27": "svamprisotto",
+	"2025-04-28": "laxinoa",
+	"2025-04-29": "sötpotatisbowl",
+	"2025-04-30": "ugnsbakad-lax",
 };
 
 export function getMenu(from?: string): Menu {
