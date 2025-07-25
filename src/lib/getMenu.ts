2efc11fdf622b8f67023198763426e44cd2455cd
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2025-07-25 09:36:53 +0200

    chore: enable dependabot, pr checking

--- src/lib/getMenu.ts
+++ src/lib/getMenu.ts
@@ -1,25 +1,25 @@
-import type { Menu } from "./types/menu";
+import type { Menu } from './types/menu';
 
 const MENU: Menu = {
-	"2025-07-23": "broccoligratäng",
-	"2025-07-24": "linsgryta",
-	"2025-07-25": "currykyckling",
-	"2025-07-26": "ugnsbakad-lax",
-	"2025-07-27": "spenat-och-fetaostpaj",
-	"2025-07-28": "tonfisksallad",
+  '2025-07-23': 'broccoligratäng',
+  '2025-07-24': 'linsgryta',
+  '2025-07-25': 'currykyckling',
+  '2025-07-26': 'ugnsbakad-lax',
+  '2025-07-27': 'spenat-och-fetaostpaj',
+  '2025-07-28': 'tonfisksallad'
 };
 
 export function getMenu(from?: string): Menu {
-	if (from) {
-		const fromDate = new Date(from);
-		const menu: Menu = {};
-		for (const [date, recipe] of Object.entries(MENU)) {
-			if (new Date(date) >= fromDate) {
-				menu[date] = recipe;
-			}
-		}
-		return menu;
-	}
+  if (from) {
+    const fromDate = new Date(from);
+    const menu: Menu = {};
+    for (const [date, recipe] of Object.entries(MENU)) {
+      if (new Date(date) >= fromDate) {
+        menu[date] = recipe;
+      }
+    }
+    return menu;
+  }
 
-	return MENU;
+  return MENU;
 }
