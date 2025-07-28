037a0e3103d56db4074844a844123ee5eb894e3d
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2025-07-28 16:59:17 +0200

    feat: plan week

--- src/lib/getMenu.ts
+++ src/lib/getMenu.ts
@@ -1,25 +1,25 @@
-import type { Menu } from './types/menu';
+import type { Menu } from "./types/menu";
 
 const MENU: Menu = {
-  '2025-07-23': 'broccoligratäng',
-  '2025-07-24': 'linsgryta',
-  '2025-07-25': 'currykyckling',
-  '2025-07-26': 'ugnsbakad-lax',
-  '2025-07-27': 'spenat-och-fetaostpaj',
-  '2025-07-28': 'tonfisksallad'
+	"2025-07-31": "ugnsbakad-falukorv",
+	"2025-08-01": "linsgryta",
+	"2025-08-02": "currykyckling",
+	"2025-08-03": "ugnsbakad-lax",
+	"2025-08-04": "pestolasagne",
+	"2025-08-05": "grekisk-sallad",
 };
 
 export function getMenu(from?: string): Menu {
-  if (from) {
-    const fromDate = new Date(from);
-    const menu: Menu = {};
-    for (const [date, recipe] of Object.entries(MENU)) {
-      if (new Date(date) >= fromDate) {
-        menu[date] = recipe;
-      }
-    }
-    return menu;
-  }
+	if (from) {
+		const fromDate = new Date(from);
+		const menu: Menu = {};
+		for (const [date, recipe] of Object.entries(MENU)) {
+			if (new Date(date) >= fromDate) {
+				menu[date] = recipe;
+			}
+		}
+		return menu;
+	}
 
-  return MENU;
+	return MENU;
 }
