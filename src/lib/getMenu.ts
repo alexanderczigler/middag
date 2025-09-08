cd0fa353bb4c14486643e2e1cf650be7db12aa5c
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2025-09-08 11:46:08 +0200

    feat(menu): swap days

--- src/lib/getMenu.ts
+++ src/lib/getMenu.ts
@@ -1,24 +1,24 @@
-import type { Menu } from './types/menu';
+import type { Menu } from "./types/menu";
 
 const MENU: Menu = {
-  '2025-09-07': 'mexicali',
-  '2025-09-08': 'laxokado',
-  '2025-09-09': 'sötpotatisbowl',
-  '2025-09-10': 'chana-daal',
-  '2025-09-11': 'broccoligratäng'
+	"2025-09-08": "mexicali",
+	"2025-09-09": "laxokado",
+	"2025-09-10": "sötpotatisbowl",
+	"2025-09-11": "broccoligratäng",
+	"2025-09-12": "chana-daal",
 };
 
 export function getMenu(from?: string): Menu {
-  if (from) {
-    const fromDate = new Date(from);
-    const menu: Menu = {};
-    for (const [date, recipe] of Object.entries(MENU)) {
-      if (new Date(date) >= fromDate) {
-        menu[date] = recipe;
-      }
-    }
-    return menu;
-  }
+	if (from) {
+		const fromDate = new Date(from);
+		const menu: Menu = {};
+		for (const [date, recipe] of Object.entries(MENU)) {
+			if (new Date(date) >= fromDate) {
+				menu[date] = recipe;
+			}
+		}
+		return menu;
+	}
 
-  return MENU;
+	return MENU;
 }
