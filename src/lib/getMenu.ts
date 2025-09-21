d2df5a97dbabb4a7203525411c22f4ecbee916fc
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2025-09-21 10:12:29 +0200

    fix: order

--- src/lib/getMenu.ts
+++ src/lib/getMenu.ts
@@ -1,13 +1,13 @@
 import type { Menu } from "./types/menu";
 
 const MENU: Menu = {
-	"2025-09-22": "ugnsbakad-lax",
+	"2025-09-22": "pokebowl",
 	"2025-09-23": "chicken-tikka-masala",
 	"2025-09-24": "chana-daal",
 	"2025-09-25": "laxokado",
 	"2025-09-26": "svamprisotto",
 	"2025-09-27": "gnocchi-köttfärssås",
-	"2025-09-28": "pokebowl",
+	"2025-09-28": "ugnsbakad-lax",
 };
 
 export function getMenu(from?: string): Menu {
