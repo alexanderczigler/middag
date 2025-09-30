606d8fa38323168b5f0abf047fd62cd9dee46bf7
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2025-09-30 20:04:37 +0200

    feat: plan week

--- src/lib/getMenu.ts
+++ src/lib/getMenu.ts
@@ -1,13 +1,12 @@
 import type { Menu } from "./types/menu";
 
 const MENU: Menu = {
-	"2025-09-22": "pokebowl",
-	"2025-09-23": "chicken-tikka-masala",
-	"2025-09-24": "chana-daal",
-	"2025-09-25": "laxokado",
-	"2025-09-26": "svamprisotto",
-	"2025-09-27": "gnocchi-köttfärssås",
-	"2025-09-28": "ugnsbakad-lax",
+	"2025-10-01": "gnocchi-köttfärssås",
+	"2025-10-02": "ugnsbakad-lax",
+	"2025-10-03": "svamprisotto",
+	"2025-10-04": "chana-daal",
+	"2025-10-05": "laxokado",
+	"2025-10-06": "pokebowl",
 };
 
 export function getMenu(from?: string): Menu {
