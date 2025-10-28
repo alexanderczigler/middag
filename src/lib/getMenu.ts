15e86eb2247a639ec95fe69d974e16b61824a897
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2025-10-28 16:14:51 +0100

    feat(menu): re-plan

--- src/lib/getMenu.ts
+++ src/lib/getMenu.ts
@@ -2,12 +2,13 @@ import type { Menu } from './types/menu';
 
 const MENU: Menu = {
   '2025-10-26': 'spenat-och-fetaostpaj',
-  '2025-10-27': 'sötpotatisbowl',
-  '2025-10-28': 'krämig-lax',
-  '2025-10-29': 'chana-daal',
+  '2025-10-27': 'gnocchi-köttfärssås',
+  '2025-10-28': 'sötpotatisbowl',
+  '2025-10-29': 'krämig-lax',
   '2025-10-30': 'pokebowl',
   '2025-10-31': 'tacokväll',
-  '2025-11-01': 'ugnsbakad-lax'
+  '2025-11-01': 'ugnsbakad-lax',
+  '2025-11-02': 'chana-daal'
 };
 
 export function getMenu(from?: string): Menu {
