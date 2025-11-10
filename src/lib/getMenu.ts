c8314fc0ed2a7b4b4523a02468d78ad70345b8ac
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2025-11-10 20:05:22 +0100

    chore(menu): plan week

--- src/lib/getMenu.ts
+++ src/lib/getMenu.ts
@@ -1,12 +1,12 @@
 import type { Menu } from './types/menu';
 
 const MENU: Menu = {
-  '2025-11-03': 'chicken-tikka-masala',
-  '2025-11-05': 'ugnsbakad-lax',
-  '2025-11-06': 'pasta-pesto',
-  '2025-11-07': 'svamprisotto',
-  '2025-11-08': 'laxinoa',
-  '2025-11-10': 'chana-daal'
+  '2025-11-11': 'tonfisksallad',
+  '2025-11-12': 'mexicali',
+  '2025-11-13': 'gnocchi-köttfärssås',
+  '2025-11-14': 'laxinoa',
+  '2025-11-15': 'pasta-pesto',
+  '2025-11-16': 'sötpotatisbowl'
 };
 
 export function getMenu(from?: string): Menu {
