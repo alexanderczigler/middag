6701c448047e2c79dd62e471c0d2c5cd3a51acb6
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2025-12-05 10:42:22 +0100

    feat: plan

--- src/lib/getMenu.ts
+++ src/lib/getMenu.ts
@@ -1,12 +1,12 @@
 import type { Menu } from './types/menu'
 
 const MENU: Menu = {
-  '2025-11-30': 'halloumi-potatis-tzatziki',
-  '2025-12-01': 'chana-daal',
-  '2025-12-02': 'ugnsbakad-lax',
-  '2025-12-03': 'gnocchi-köttfärssås',
-  '2025-12-04': 'laxinoa',
-  '2025-12-05': 'tacokväll'
+  '2025-12-07': 'halloumi-potatis-tzatziki',
+  '2025-12-08': 'currykyckling',
+  '2025-12-09': 'ugnsbakad-lax',
+  '2025-12-10': 'mexicali',
+  '2025-12-11': 'chana-daal',
+  '2025-12-12': 'svamprisotto'
 }
 
 export function getMenu(from?: string): Menu {
