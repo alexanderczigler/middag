09242c799c3de39b4a6b71fafeba67aad05ac177
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2026-02-09 10:44:51 +0100

    feat: plan week

--- src/lib/getMenu.ts
+++ src/lib/getMenu.ts
@@ -1,14 +1,12 @@
 import type { Menu } from './types/menu'
 
 const MENU: Menu = {
-  '2026-02-02': 'ugnsbakad-lax',
-  '2026-02-03': 'pokebowl',
-  '2026-02-04': 'halloumi-potatis-tzatziki',
-  '2026-02-05': 'pasta-pesto',
-  '2026-02-06': 'spenat-och-fetaostpaj'
+  '2026-02-10': 'thaiwok-kyckling',
+  '2026-02-11': 'mexicali',
+  '2026-02-12': 'laxinoa',
+  '2026-02-13': 'ugnsbakad-lax',
+  '2026-02-14': 'krämig-lax'
 }
-
-// TODO: Next week, add the chicken thai wok!
 
 export function getMenu(from?: string): Menu {
   if (from) {
